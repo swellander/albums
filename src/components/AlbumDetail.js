7f5f6766a4f09b7bb62a312cac118d1afdd7be57
Samuel Wellander <samwellander@gmail.com>
2017-05-15 19:34:04 -0700

    Style Button component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button'
 
 
 const AlbumDetail = ({ album }) => {
@@ -34,11 +35,16 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image
           style={imageStyle}
           source={{ uri: image }}
         />
+      </CardSection>
+
+      <CardSection>
+        <Button />
       </CardSection>
 
     </Card>
